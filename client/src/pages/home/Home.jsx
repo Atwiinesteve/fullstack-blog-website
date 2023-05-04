2ae27023bc28f51775507abf3f97186d3c324065
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-05-04 13:44:06 +0000

    completed the write post component with styling

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -6,9 +6,12 @@ import Header from '../../components/header/Header';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Posts from '../../components/posts/Posts';
 
+// import pages
+import SinglePost from '../single/SinglePost';
+import Write from '../write/Write';
+
 // styles
 import "./home.css";
-import Single from '../../components/single/Single';
 
 // home component
 export default function Home() {
@@ -17,8 +20,9 @@ export default function Home() {
         <Header />
         <div className="main__body">
           {/* <Posts /> */}
-          <Single />
-          <Sidebar />
+          {/* <SinglePost /> */}
+          <Write />
+          {/* <Sidebar /> */}
         </div>
     </>
   )
