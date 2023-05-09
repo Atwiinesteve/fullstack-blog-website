e2d64d13f3a9467d451188b1185826cafc7e053b
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-05-09 15:47:58 +0300

    added conditional routes

--- client/src/App.js
+++ client/src/App.js
@@ -11,17 +11,24 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Settings from "./pages/settings/Settings";
 import Register from "./pages/register/Register";
+import Write from "./pages/write/Write";
+import SinglePost from "./pages/single/SinglePost";
 
 // app component.
 export default function App() {
+
+  const user = false;
+
 	return (
 		<>
 			<Topbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/update" element={<Settings />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
+				<Route path="/settings" element={ user ? <Settings /> : <Register />} />
+				<Route path="/login" element={ user ? <Home /> : <Login />} />
+				<Route path="/register" element={ user ? <Home /> : <Register />} />
+				<Route path="/write" element={ user ? <Write /> : <Register />} />
+				<Route path="/blog" element={ user ? <SinglePost /> : <Register />} />
 			</Routes>
 		</>
 	);
