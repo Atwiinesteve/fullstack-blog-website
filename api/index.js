39e64e02d96bffbca2a25dfc6f6ccbbde8e12bcc
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-05-12 18:24:00 +0300

    corrected file state error

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 require("dotenv/config");
 
 require("./database/database.connections");
@@ -19,6 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '/images')))
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", require("./routes/auth.route"));
