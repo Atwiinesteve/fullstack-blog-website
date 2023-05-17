9bf765075dc37587c4a8fc4082b99bd59e0e295f
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-05-17 18:15:59 +0300

    added update functionality to the client

--- client/src/components/single/Single.jsx
+++ client/src/components/single/Single.jsx
@@ -3,16 +3,14 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Context } from "../../context/Context";
 
-
 // import styles
 import "./single.css";
 import axios from "axios";
 
 // single post component
 export default function Single() {
-	
 	// user context
-	const {user} = useContext(Context);
+	const { user } = useContext(Context);
 
 	// image url
 	const PF = "http://localhost:8080/images/";
@@ -23,14 +21,33 @@ export default function Single() {
 
 	// single post state
 	const [post, setPost] = useState({});
+	const [title, setTitle] = useState("");
+	const [desc, setDesc] = useState("");
+	const [updateMode, setUpdateMode] = useState(false);
 
 	// handle delete function
 	const handleDelete = async () => {
 		try {
-			await axios.delete(`/delete-post/${post._id}`, { data: { username: user.username }});
-			window.location.replace("/");
+			await axios.delete(`/delete-post/${post._id}`, {
+				data: { username: user.username }
+			});
+			window.location.reload();
 		} catch (error) {
-			console.log(error)
+			console.log(error);
+		}
+	};
+
+	// handle update
+	const handleUpdate = async () => {
+		try {
+			await axios.put(`/update-post/${post._id}`, {
+				username: user.username,
+				title,
+				desc,
+			});
+			setUpdateMode(false);
+		} catch (error) {
+			console.log(error);
 		}
 	}
 
@@ -39,6 +56,8 @@ export default function Single() {
 		const fetchPost = async () => {
 			const result = await axios.get(`/post/${path}`);
 			setPost(result.data.post);
+			setTitle(result.data.post.title);
+			setDesc(result.data.post.desc);
 		};
 		fetchPost();
 	}, [path]);
@@ -50,17 +69,38 @@ export default function Single() {
 					<img className="single__post__img" src={PF + post.photo} alt="" />
 				)}
 
-				<h1 className="single__post__title">
-					{post.title}
-					{post.username === user.user?.username && (
-						<div className="single__post__edit">
-							<i className="single__post__icon far fa-edit"></i>
-							<i
-								className="single__post__icon far fa-trash-alt"
-								onClick={handleDelete}></i>
-						</div>
-					)}
-				</h1>
+				{updateMode ? (
+					<input
+						name=""
+						style={{
+							width: "98.8%",
+							border: "1px solid lightgray",
+							borderRadius: "1px",
+							outline: "none",
+							padding: "0.5rem",
+							textAlign: "center"
+						}}
+						type="text"
+						value={title}
+						autoFocus
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				) : (
+					<h1 className="single__post__title">
+						{title}
+						{post.username === user.user?.username && (
+							<div className="single__post__edit">
+								<i
+									className="single__post__icon far fa-edit"
+									onClick={() => setUpdateMode(true)}></i>
+								<i
+									className="single__post__icon far fa-trash-alt"
+									onClick={handleDelete}></i>
+							</div>
+						)}
+					</h1>
+				)}
+
 				<div className="single__post__info">
 					<span>
 						Author:
@@ -72,7 +112,42 @@ export default function Single() {
 					</span>
 					<span>{new Date(post.createdAt).toDateString()}</span>
 				</div>
-				<p className="single__post__desc">{post.desc}</p>
+
+				{updateMode ? (
+					<textarea
+						style={{
+							width: "97.5%",
+							border: "1px solid lightgray",
+							borderRadius: "1px",
+							height: "450px",
+							outline: "none",
+							padding: "1rem",
+						}}
+						value={desc}
+						onChange={(e) => setDesc(e.target.value)}
+					/>
+				) : (
+					<p className="single__post__desc">{desc}</p>
+				)}
+
+				{updateMode && (
+					<button
+						style={{
+							border: "none",
+							padding: "0.8rem 1rem",
+							outline: "none",
+							cursor: "pointer",
+							backgroundColor: "green",
+							fontFamily: "Roboto",
+							color: "white",
+							borderRadius: "1px",
+							letterSpacing: "1px",
+							textTransform: "uppercase",
+						}}
+						onClick={handleUpdate}>
+						Update post
+					</button>
+				)}
 			</div>
 		</div>
 	);
